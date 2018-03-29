fa44e03f07e5f6588e525a8bc5182f8ab25e1674
arifstwan <arif.stwan@gmail.com>
2018-03-29 03:42:22 +0000

    hp

--- gray.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gs -sOutputFile=converted.pdf -sDEVICE=pdfwrite -sColorConversionStrategy=/Gray -dPDFUseOldCMS=false -dProcessColorModel=/DeviceGray -dCompatibiltyLevel=1.4 -dNOPAUSE -dBATCH $@ 
-
